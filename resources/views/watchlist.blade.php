fa92ce1cda57bdf055445ccfcd3fd58805578f49
sines210 <sines210@gmail.com>
2021-04-19 19:20:59 +0200

    chg front wlidt

--- resources/views/watchlist.blade.php
+++ resources/views/watchlist.blade.php
@@ -6,24 +6,30 @@
 
     <article class="watch_list">
     
-        <div class="anime--header">
+        <ul class="w_list">
         
         @foreach($animes as $anime)
-        <span >
-            <strong style="margin: 3%;"> {{$anime->w_title}}</strong>
-               <img alt="" style="width:15vw; height:20vh; margin: 2% 5%" src="/covers/{{$anime->w_cover}}" />
-        </span>
-        <span >
-             <form action="deleteFromWatchList" method="POST">
-                <input type="hidden" name="watchListId" value="{{$anime->cov_id}}"/>
-                <button type="submit" class="cta" value="supprimer">Supprimer</button>
-                @csrf
-             </form>
-          </span>
+        <div class="anime--header">
+            <span>
+                <li>
+                  <strong style="margin: 3%;"> {{$anime->w_title}}</strong>
+                   <img alt="" style="width:15vw; height:20vh; margin: 2% 5%" src="/covers/{{$anime->w_cover}}" />
+                </li>
+            </span>
+            <span>
+                 <li class="deleteWatchAnime" style="list-style-type: none;">
+                     <form action="deleteFromWatchList" method="POST">
+                          <input type="hidden" name="watchListId" value="{{$anime->cov_id}}"/>
+                         <button type="submit" class="cta" value="supprimer">Supprimer</button>
+                         @csrf
+                    </form>
+                 </li>
+             </span>
+          </div>
 
         @endforeach
         
-        </div>
+        </ul>
     
     </article>
 
